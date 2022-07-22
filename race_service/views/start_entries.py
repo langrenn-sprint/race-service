463c35364e34a68b421d5eac6b8f6a400fe2219f
heming-langrenn <56455987+heming-langrenn@users.noreply.github.com>
2022-07-22 14:40:18 +0200

    New get start entries (#101)

--- race_service/views/start_entries.py
+++ race_service/views/start_entries.py
@@ -55,6 +55,9 @@ class StartEntriesView(View):
                     db, race_id, startlist_id
                 )
             )
+        elif "bib" in self.request.rel_url.query:
+            bib = int(self.request.rel_url.query["bib"])
+            start_entries = await StartEntriesService.get_start_entries_by_bib(db, bib)
         else:
             start_entries = await StartEntriesService.get_start_entries_by_race_id(
                 db, race_id
